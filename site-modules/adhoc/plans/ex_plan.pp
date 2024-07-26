baa548e8392002e179b2eb2b0cdc94ffd03b5a63
Garrett Rowell <garrett@puppet.com>
2024-07-26 08:50:01 -0600

    asdf

--- site-modules/adhoc/plans/ex_plan.pp
+++ site-modules/adhoc/plans/ex_plan.pp
@@ -3,12 +3,6 @@ plan adhoc::ex_plan (
 ) {
   $result_or_error = run_task('adhoc::example', $targets, '_catch_errors' => true, 'message' => 'first task')
 
-  #  $result = if $result_or_error =~ Error {
-  #    # print message
-  #    out::message('task 1 failed')
-  #  } else {
-  #    $result_or_error
-  #  }
   $result = case $result_or_error {
     # When the plan returned a ResultSet use it.
     ResultSet: { $result_or_error }
