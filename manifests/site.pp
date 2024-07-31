9a0b4db855992337041bf8f9980c99db8e68e299
Garrett Rowell <garrett@puppet.com>
2024-07-31 16:13:09 -0600

    asdf

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,9 +48,9 @@ node default {
 node 'osp-primary.garrett.rowell' {
   class { 'puppet_status_check':
     role                 => 'primary',
-    indicator_exclusions => [
-      'S0007', 'S0011', 'S0010',
-      'S0011', 'S0027', 'S0029'
-    ],
+    #    indicator_exclusions => [
+    #      'S0007', 'S0011', 'S0010',
+    #      'S0011', 'S0027', 'S0029'
+    #    ],
   }
 }
